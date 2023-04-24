8708ff398891810bc6eebb24f7eb0683519374e3
Shirshak <shirshaktiwari2021@gmail.com>
2023-04-24 12:44:45 +0545

    Database Configured

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'firstProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learndjango',
+        'USER': 'postgres',
+        'PASSWORD': 'planet2058',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
